c02a1dae15d2fffbc6490ec4d5ed6fd7c8e26429
alex <aliabuharb8530@gmail.com>
2018-11-11 10:49:56 +0200

    edit2

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -15,7 +15,9 @@ import android.widget.Toast;
 
 import com.example.androidjokelib.jokeActivity;
 import com.example.libjoke.MyJoke;
-import com.google.api.client.extensions.android.http.AndroidHttp;
+
+
+ import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
@@ -83,16 +85,15 @@ class EndpointAsyncTask extends AsyncTask<MainActivityFragment, Void, String> {
 
 
         if(myApiService == null) {  // Only do this once
-            MyApi.Builder builder = new
-                    MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
-
-
-
+                    // options for running against local devappserver
+                    // - 10.0.2.2 is localhost's IP address in Android emulator
+                    // - turn off compression when running against local devappserver
                     .setRootUrl("http://10.0.2.2:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                        @Override                         public void initialize(AbstractGoogleClientRequest<?>
-                                                                  abstractGoogleClientRequest) throws IOException {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
                             abstractGoogleClientRequest.setDisableGZipContent(true);
                         }
                     });
@@ -106,6 +107,7 @@ class EndpointAsyncTask extends AsyncTask<MainActivityFragment, Void, String> {
         } catch (IOException e) {
             return e.getMessage();
         }
+
     }
 
     @Override
@@ -118,43 +120,3 @@ class EndpointAsyncTask extends AsyncTask<MainActivityFragment, Void, String> {
 
     }
 }
-//class EndpointAsyncTask extends AsyncTask<Context, Void, String> {
-//    private static MyApi myApiService = null;
-//    private Context context;
-//
-//    @Override
-//    protected String doInBackground(Context... params) {
-//        if(myApiService == null) {  // Only do this once
-//            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-//                    new AndroidJsonFactory(), null)
-//                    // options for running against local devappserver
-//                    // - 10.0.2.2 is localhost's IP address in Android emulator
-//                    // - turn off compression when running against local devappserver
-//                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-//                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-//                        @Override
-//                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-//                            abstractGoogleClientRequest.setDisableGZipContent(true);
-//                        }
-//                    });
-//            // end options for devappserver
-//
-//            myApiService = builder.build();
-//        }
-//
-//        context = params[0];
-//
-//
-//        try {
-//            return myApiService.makeJoke().execute().getData();
-//        } catch (IOException e) {
-//            return e.getMessage();
-//        }
-//    }
-//
-//    @Override
-//    protected void onPostExecute(String result) {
-//
-//        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
-//    }
-//}
